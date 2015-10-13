5de0afb1f906670f1ea74126bf6984eef88b07d4
Zui <zuikng@gmail.com>
2015-10-12 20:55:47 -0400

    Mistaken StringProblem

---
+++ StringProblem.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class StringProblem{
+  public static void main(String[] args){
+    
+  }
+
+// Method returns the number of spaces in str.
+  public static int countSpaces(String str){
+   
+  }
+//count the number of times b occurs in a.
+  public static int countWords(String a, String b){
+
+  }
+//return true if str has the same number of occurences of 
+// "cat" and "dog". 
+// Must Use countWords!!
+  public static boolean catDog(String str){
+    
+  }
+}
