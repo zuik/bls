64378ce7361b9e27df9d2f007bd8a35949451197
Zui <zuikng@gmail.com>
2015-10-19 04:42:50 -0400

    Finish Random Generator problems

--- RandomGenerator.java
+++ RandomGenerator.java
@@ -1,7 +1,11 @@
 import java.util.*;
 public class RandomGenerator {
 	public static void main(String[] args) {
-		System.out.print("Count: "+randomEven(500));
+		for(int i=0; i <100; i++){
+			System.out.println(round10(i));
+		}
+		//System.out.println(round10(5));
+		//System.out.println(0%10);
 	}
 
 	// generate and print 10 integers from the range 0 to n inclusive
@@ -18,28 +22,63 @@ public class RandomGenerator {
 		return counter;
 
 	}
-/*	// generate and print the results of m random coin tosses
+	// generate and print the results of m random coin tosses
 	// print out the number of heads and the number of tails.
 	// Sample output: coinToss(8)
 	// H T H H T T H H
 	// Number of Heads: 5
 	// Number of Tails: 3
 	public static void coinToss(int m) {
-	return "";
+		ArrayList<Integer> coinList = new ArrayList<Integer>();
+		for (int i = 0; i<m; i++){
+			int x = (int) (2*Math.random());
+			coinList.add(x);
+		}
+		for (Integer i: coinList){
+			if(i == 1){
+				System.out.print("H ");
+			} else {
+				System.out.print("T ");
+			}
+		}
+		int sum = sumList(coinList);
+		System.out.println("\nNumber of heads: "+sum);
+		System.out.println("Number of tails: "+ (coinList.size()-sum));
 	}
-
-	//For this problem, we'll round an int value up to the next multiple of 10 
-	//if its rightmost digit is 5 or more, so 15 rounds up to 20. 
-	//Alternately, round down to the previous multiple of 10 if its rightmost digit 
-	//is less than 5, so 12 rounds down to 10. 
+	/**
+	* A terrible way to generate an INTEGER sum of a
+	* INTEGER ArrayList
+	* @param inputList : take ArrayList to be summed
+	* @return Integer sum of the list
+	*/
+	public static int sumList(ArrayList<Integer> inputList){
+		int result = 0;
+		for(Integer i: inputList){
+			result += i;
+		}
+		return result;
+	}
+	//For this problem, we'll round an int value up to the next multiple of 10
+	//if its rightmost digit is 5 or more, so 15 rounds up to 20.
+	//Alternately, round down to the previous multiple of 10 if its rightmost digit
+	//is less than 5, so 12 rounds down to 10.
 	public static int round10(int n) {
-	return "";
+		int holder = n;
+		int countStep = 0;
+		while(!(holder%10==0)){
+			holder--;
+			countStep++;
+		}
+		//System.out.println(countStep);
+		if(countStep>=5)
+			return holder+10;
+		return holder;
 	}
 
 
 	// return the sum of the rounded values of a,b, and c
 	// You must use round10 above.
 	public static int roundSum(int a, int b, int c) {
-	return "";
-	}*/
+		return round10(a)+round10(b)+round10(c);
+	}
 }
