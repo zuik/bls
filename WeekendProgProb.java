1c0bdf7b7af9044b9b2acb50c522eda42089d0db
Zui <zuikng@gmail.com>
2015-10-27 11:59:57 -0400

    Finished to P2

--- WeekendProgProb.java
+++ WeekendProgProb.java
@@ -1,64 +1,78 @@
-public class WeekendProgProb{
-  public static void main(String[] ag){
-    //System.out.println(twoConsecutive(1,2,2));
-    //System.out.println(twoConsecutive(1,2,12));
-    //System.out.println(twoConsecutive(1,3,5));
-    //fourHeads();
-    //nHeads(2);
-    //int howMuch = Integer.parseInt(ag[0]);
-    //nHeads(howMuch);
-    //System.out.print("asdfhashasfd ".contains("has"));
-    printTwoDigit(50);
-  }
-  public static boolean twoConsecutive(int a, int b, int c){
-    return Math.abs(a-b)==1 || Math.abs(a-c)==1 || Math.abs(c-b)==1;
-  }
-  public static String coinFlip(){
-    if ((int) (2*Math.random()) == 0){
-      return ("H ");
-    } else{
-      return("T ");
-    }
-  }
-  public static void fourHeads(){
-    int count = 0;
-    while (count<4){
-      String dummy = coinFlip();
-      System.out.print(dummy);
-      if(dummy =="H ")
-        count++;
-      else
-        count = 0;
-    }
-    System.out.println("\n Your sequence has had 4 heads");
-  }
-  public static void nHeads(int n){
-    int count = 0;
-    while (count<n){
-      String dummy = coinFlip();
-      //System.out.print(dummy);
-      if(dummy =="H ")
-        count++;
-      else
-        count = 0;
-    }
-    System.out.println("\n Your sequence has had "+ n +" heads");
-  }
-  public static void printTwoDigit(int n){
-    boolean fourtyTwo = false;
-    for(int i=0; i<n; i++){
-      int num = (int) (90*Math.random())+10;
-      System.out.println("next = " + num);
-      if(num==42){
-        fourtyTwo=true;
-      }
-    }
-    if (fourtyTwo)
-      System.out.println("We saw a 42!");
-    else
-      System.out.println("No 42 was seen.");
-  }
-  /* Below is a complicated version of what should be simple
+import java.util.Scanner;
+
+public class WeekendProgProb {
+	public static void main(String[] ag) {
+		//System.out.println(twoConsecutive(1,2,2));
+		//System.out.println(twoConsecutive(1,2,12));
+		//System.out.println(twoConsecutive(1,3,5));
+		//fourHeads();
+		//nHeads(2);
+		//int howMuch = Integer.parseInt(ag[0]);
+		//nHeads(howMuch);
+		//System.out.print("asdfhashasfd ".contains("has"));
+		Scanner console = new Scanner(System. in );
+		//printTwoDigit(50);
+		//favoriteLetter(console, "a");
+		favoriteLetter(console, "z");
+	}
+	public static boolean twoConsecutive(int a, int b, int c) {
+		return Math.abs(a - b) == 1 || Math.abs(a - c) == 1 || Math.abs(c - b) == 1;
+	}
+	public static String coinFlip() {
+		if ((int)(2 * Math.random()) == 0) {
+			return ("H ");
+		} else {
+			return ("T ");
+		}
+	}
+	public static void fourHeads() {
+		int count = 0;
+		while (count < 4) {
+			String dummy = coinFlip();
+			System.out.print(dummy);
+			if (dummy == "H ") count++;
+			else count = 0;
+		}
+		System.out.println("\n Your sequence has had 4 heads");
+	}
+	public static void nHeads(int n) {
+		int count = 0;
+		while (count < n) {
+			String dummy = coinFlip();
+			//System.out.print(dummy);
+			if (dummy == "H ") count++;
+			else count = 0;
+		}
+		System.out.println("\n Your sequence has had " + n + " heads");
+	}
+	public static void printTwoDigit(int n) {
+		boolean fourtyTwo = false;
+		for (int i = 0; i < n; i++) {
+			int num = (int)(90 * Math.random()) + 10;
+			System.out.println("next = " + num);
+			if (num == 42) {
+				fourtyTwo = true;
+			}
+		}
+		if (fourtyTwo) System.out.println("We saw a 42!");
+		else System.out.println("No 42 was seen.");
+	}
+	public static void favoriteLetter(Scanner input, String x) {
+		int ctr = 0;
+		String holder;
+		String clearOut = (char) 27 + "[0m";
+		String redOut = (char) 27 + "[4m";
+		System.out.println("Looking for: " + x);
+		while (ctr < 2) {
+			System.out.print(clearOut + "Type a word: " + redOut);
+			holder = input.nextLine();
+			System.out.println(clearOut + "Got:" + holder);
+			if (holder.indexOf(x) == 0) ctr++;
+			else ctr = 0;
+			if (ctr == 2) System.out.println(clearOut + "Terminated at:" + holder + "/" + x);
+		}
+	}
+	/* Below is a complicated version of what should be simple
   public static void fourHeads(){
     String counter = "";
     int cc = 0;
