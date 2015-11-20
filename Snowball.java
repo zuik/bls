b55f928d2928ca03882463e1d2a83f1581206bf5
Zui Nguyen <zuikng@gmail.com>
2015-11-19 20:04:50 -0800

    Sadly this will cause a StackOverflow error.

--- Snowball.java
+++ Snowball.java
@@ -1,8 +1,8 @@
 public class Snowball{
   public static void main(String[] agr){
-    System.out.println(Func(22));
+    System.out.println(search(1,100000,0));
   }
-  public static int Func(int n){
+  public static int func(int n){
     int count = 0;
     while (n > 1){
       if (n%2==0){
@@ -13,6 +13,15 @@ public class Snowball{
         count ++;
       }
     }
-    return n+1;
+    return count+1;
+  }
+  public static int search(int a, int b, int m){
+    int x = func(a);
+    if (x>m)
+      m = x;
+    if (a==b)
+      return m;
+    else
+      return search(a+1,b,m);
   }
 }
