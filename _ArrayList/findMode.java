deb1fbb83961be22280dbf3fb7a92fe9a03c8a49
Zui <zuikng@gmail.com>
2016-04-15 12:36:16 -0400

    Update after organising some files

---
+++ _ArrayList/findMode.java
@@ -0,0 +1,30 @@
+public class findMode{
+    public static void main(String[] args){
+
+    }
+    public static int mode(int[] array){
+        ArrayList<Integer> r = new ArrayList<>();
+
+    }
+    public static int find(List<Value> list, target){
+
+    }
+}
+class Value {
+    private int value;
+    private int counter;
+    public Value(int v) {
+        value=v;
+        count=0;
+    }
+    public int getValue(){
+
+    }
+    public int getCounter(){
+
+    }
+    //increment counter by 1
+    public void increment(){
+
+    }
+} 
